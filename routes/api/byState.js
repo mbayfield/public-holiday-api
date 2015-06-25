474a384c431f890dd5887d285cb8c55818eb69fc
mbayfield <mbayfield@gmail.com>
2015-06-25 23:05:31 +1000

    clean the first api + add an example link

--- routes/api/byState.js
+++ routes/api/byState.js
@@ -6,26 +6,17 @@ exports = module.exports = function(req, res) {
 
 	var PublicHoliday = keystone.list('PublicHoliday');
 	
-	console.log(new Date(req.query.date + ' 00:00:00'));
 	var query = PublicHoliday.model.find({state: req.params.state, date: {$eq: new Date(req.query.date + ' 00:00:00')}});
-		// query.select('_id name');
-		// query.limit(20);
-		query.exec(function(err, data) {
-			console.log(err);
-			console.log(data);
-			if (err || !data) {
+		// query.select('name moreInformation description state date');
+		query.exec(function(err, result) {
+			if (err || !result) {
 				return res.json({
 					success: false,
 					err: err
 				});
 			} else {
-				// remap the results
-				// remapListings = _.map(listings, function(i) {
-				// 	return { _id: i._id, name: i.name }
-				// });
 				return res.json({
-					success: true,
-					data: data
+					data: result
 				});
 			}
 		});
